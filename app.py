29d4094ae48d161dcc35ecee6b3160af4fd283fa
Bahamyirou <asma.bahamyirou@umontreal.ca>
2025-12-15 22:06:53 -0500

    commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 pages = {
     "Pages": [
         st.Page("./views/AboutPage.py", title="About", icon="⚠️", default=True),
-        st.Page("./views/Page1.py", title="Population by prefecture and gender", icon="📊"),
+        st.Page("./views/page1.py", title="Population by prefecture and gender", icon="📊"),
         st.Page("./views/page2.py", title="Prefecture Boundary Viewer", icon="🗺️"),
        # st.Page("./views/page1.py", title="Togolese prefecture page", icon="🚰"),
        # st.Page("./views/page2.py", title="Resident Population by sexe", icon="⚖️"),
