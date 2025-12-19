45baab05b73844ad4071b8ab5b464bacf9a1cfe6
AsmaBahamyirou <asma.bahamyirou@umontreal.ca>
2025-12-19 09:27:43 -0500

    commit

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ pages = {
     "Pages": [
         st.Page("./views/AboutPage.py", title="About", icon="⚠️", default=True),
         st.Page("./views/page1.py", title="Population by prefecture and gender", icon="📊"),
-        st.Page("./views/page2.py", title="Prefecture Boundary Viewer", icon="🗺️"),
-        st.Page("./views/Page3.py", title="Prefecture Boundary xxx", icon="🗺️"),
+        st.Page("./views/page2.py", title="Prefecture Boundary Viewer", icon="🗺️")
     ],
 }
 
