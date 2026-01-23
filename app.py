121b1d4728d4379beda4ad3970bdeaedf7bee7f0
Bahamyirou <asma.bahamyirou@umontreal.ca>
2026-01-23 18:40:47 -0500

    commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-##publish add
+##Add Pages Here
 pages = {
     "Pages": [
         st.Page("./views/AboutPage.py", title="About", icon="⚠️", default=True),
-        st.Page("./views/page4.py", title="Population Distribution", icon="🗺️"),
-        st.Page("./views/page3.py", title="Population Distribution2", icon="🗺️"),
+        #st.Page("./views/page4.py", title="Population Distribution", icon="🗺️"),
+        st.Page("./views/page3.py", title="Population Distribution", icon="🗺️"),
     ],
 }
 
