a7563f59515d6b0a07d58967155dee9a3b42861b
AsmaBahamyirou <asma.bahamyirou@umontreal.ca>
2026-01-23 15:54:50 -0500

    Clean up page entries in app.py
    Removed commented-out page entries for clarity.

--- app.py
+++ app.py
@@ -3,13 +3,7 @@ import streamlit as st
 pages = {
     "Pages": [
         st.Page("./views/AboutPage.py", title="About", icon="⚠️", default=True),
-<<<<<<< HEAD
-        ## st.Page("./views/page2.py", title="Prefecture Boundary Viewer", icon="🗺️"),
         st.Page("./views/page4.py", title="Population Distribution", icon="🗺️"),
-=======
-        st.Page("./views/page1.py", title="Population by prefecture and gender", icon="📊"),
-        st.Page("./views/page2.py", title="Prefecture Boundary Viewer", icon="🗺️")
->>>>>>> 45baab05b73844ad4071b8ab5b464bacf9a1cfe6
     ],
 }
 
